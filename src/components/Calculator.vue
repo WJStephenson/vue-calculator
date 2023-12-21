3d26d9d7856fb212b0f31ad133b0c39520f8de64
WJStephenson <willjosteph@gmail.com>
2023-12-21 21:08:04 +0000

    first commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,29 @@
+<script>
+    
+</script>
+
+<template>
+    <div class="calculator">
+        <div class="display"></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
